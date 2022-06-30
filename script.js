e6794446fa71c3d7967299f8e88059dbb4513877
Anthony LoneFight <tonylonefight@gmail.com>
2022-06-29 19:43:08 -0500

    Changeing out HTML inner to add question 1

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-function startTimer(){
+function startTimer()
+// The timer begins
+{
     var counter = 30;
-    setInterval(function() {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = counter;
-      }
-      if (counter === 0) {
+      setInterval(function() {
+        counter--;
+          if (counter >= 0) {
+          span = document.getElementById("count");
+          span.innerHTML = counter;
+          }
+          if (counter === 0) {
           alert('sorry, out of time');
           clearInterval(counter);
-      }
-    }, 1000);
-  }
-  function start()
-  {
-      document.getElementById("count").style="color:green;";
+          }
+      }, 1000);
+    var questionsEl = document.querySelector('.wrap'); 
+    console.log(questionsEl);
+
+    var quizHeaderEl = document.querySelector('.quizHeader');
+    quizHeaderEl.innerHTML = 'Question number 1';
+    
+    var initialTextEl = document.querySelector('#initialText')
+    initialTextEl.innerHTML = 'Ben Brower played John Crichtion in sci-fi television show Farscape. In what city did he grow up?';
+console.log(initialTextEl);
+}
+
+function start()
+{
+      document.getElementById("count").style="color:red;";
       startTimer();
-  };
+};
+
+
+
+{/* <div class="wrap">
+<span class=quizHeader>Coding Quiz Challenge</span>
+    <p id="initialText">Try to answer the following code-related questions within the time limit. Keep in mind that the incorrect answer will penalize your score/time by 10 seconds!</p>
+
+    <button onclick="start()">
+    Begin Quiz
+    </button>
+</div> */}
+
